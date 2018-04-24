2948a589073486268dd9793a4a76b5adb506d371
mafshar <ma2510@nyu.edu>
2018-04-23 23:14:23 -0400

    updated code for drop 2

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# deep-recommender
-Recommender System that explores traditional collaborative filtering with some deep models
+# Movie Recommendation System
+
+Mohammad Afshar, ma2510@nyu.edu
+
+## Overview
+
+### Background and Motivation
+TBD
+
+## Dataset
+
+The data that will be used will be the MovieLens dataset for movie recommendations.
+
+## Methodology
+Recommendation system will be a collaborative filtering technique using the following two methodologies:
+
+* Matrix factorization (using ALS)
+* (Optional) Autoencoders
+
+## Model
+The model that will be used to come up with baseline recommendation predictions will be a collaborative filtering technique called Alternating Least Squares (ALS). The second model (optional) that might also be explored is a deep learning collaborative filtering technique involving autoencoders.
+
+## Results
+TBD
+
+## Setup
+Setup your data by following these instructions. The download link is [here](http://files.grouplens.org/datasets/movielens/ml-20m.zip)
+
+* Locally, create a `/data` directory in the project root.
+
+* Download all data files and expand them locally in `/data`.
+
+When finished, your `/data` directory should look like this:
+
+```
+/data
+  movies.csv
+  ratings.csv
+  tags.csv
+```
+
+To run the code:
+
+```
+spark-shell -i recommender.scala
+```
--- recommender.scala
+++ recommender.scala
@@ -1,6 +1,6 @@
-import org.apache.spark.mllib.recommendation.ALS // Uses ALS to determine latent factors in the matrix
-import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
-import org.apache.spark.mllib.recommendation.Rating
+// Uses ALS to determine latent factors in the matrix
+// Use Rating class
+import org.apache.spark.mllib.recommendation._
 
 val datafile = "/users/ma2510/project/recommender/*"
 
@@ -11,11 +11,23 @@ val ratings = data
     .map(record => Rating(user.toInt, item.toInt, rate.toDouble))
 
 val usersProducts = ratings
-    .map {
-        case Rating(user, product, rate) => (user, product)
-}
+    .map(record => Array(user, product)
 
 // Model parameters
 val rank = 10
 val iters = 10
 val model = ALS.train(ratings, rank, iters, 0.01)
+
+// getting the predictions y_hat
+val predictions = model.predict(usersProducts)
+    .map(record => Array((record(0), record(1)), record(2))) // ((user, product), rate))
+
+// getting the ratings y and the predictions y_hat to calculate accuracy/precision/recall
+val ratingsAndPred = ratings
+    .map(record => Array((record(0), record(1)), record(2))) // ((user, product), rate))
+    .join(predictions)
+
+// NEXT STEPS:
+// calculate the precision and recall
+// use a metric to measure the understanding of the model and the accuracy/precision/recall
+// explore methods in deep learning in order to make a deep collaborative filtering model (autoencoders)
