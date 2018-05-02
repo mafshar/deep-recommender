edc8d454a57f869dffeff0c216a9d172bb5b1386
mafshar <ma2510@nyu.edu>
2018-05-02 14:14:37 -0400

    code refactor and optimization of splitting added

--- recommender.scala
+++ recommender.scala
@@ -1,8 +1,41 @@
-// Uses ALS to determine latent factors in the matrix
-// Use Rating class
-import org.apache.spark.mllib.recommendation._
+import org.apache.spark.mllib.recommendation._ // Uses ALS to determine latent factors in the matrix; Uses Rating class
+import org.apache.spark.sql._ // Uses spark sql datatypes (needed for parsing)
 
-val datafile = "/users/ma2510/project/recommender/*"
+// SQL Context
+val sqlc = new org.apache.spark.sql.SQLContext(sc)
+import sqlc.implicits._ // Uses sqlc datatypes
+
+// Class Definitions:
+case class Movie(movieId: Int, title: String, genres: Seq[String])
+case class User(userId: Int, gender: String, age: Int, occupation: Int, zipcode: String)
+
+// Helper functions:
+def isValidMovie(record: String) : Boolean = {
+    val delimeter : String = "::"
+    val values = record.split(delimeter)
+    values.length == 3
+}
+
+def createMovie(record: String) : Movie = {
+    val delimeter : String = "::"
+    val values = record.split(delimeter)
+    Movie(values(0).toInt, values(1).toString, Seq(values(2)))
+}
+
+def isValidUser(record: String) : Boolean = {
+    val delimeter : String = "::"
+    val values = record.split(delimeter)
+    values.length == 5
+}
+
+def createUser(record: String) : User = {
+    val delimeter : String = "::"
+    val values = record.split(delimeter)
+    User(values(0).toInt, values(1).toString, values(2).toInt, values(3).toInt, values(4).toString)
+}
+
+// datafiles:
+val ratingsfile = "/user/ma2510/movies_data/ratings.dat"
 
 val data = sc.textFile(datafile)
 
